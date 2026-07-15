41f3af4885e4606b38e6f6f598b2ae1e86edcf1c
LishaYUJ <lyuj137@aucklanduni.ac.nz>
2026-07-16 00:37:41 +1200

    feat: remove Timezone property from UserSettings and disable TimezonesController

--- backend/Controllers/TimezonesController.cs
+++ backend/Controllers/TimezonesController.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
+
+/*using Microsoft.AspNetCore.Mvc;
 
 namespace SleepyKoala.Api.Controllers
 {
@@ -60,3 +61,4 @@ namespace SleepyKoala.Api.Controllers
         }
     }
 }
+*/
---
+++ backend/Migrations/20260713124205_UpdateDatabaseSchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SleepyKoala.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timezone",
+                table: "UserSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Timezone",
+                table: "UserSettings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
